301363043fbb579403f785ea69235bc4edfdaef8
Peter Daum <daum@tyntec.com>
2019-07-01 10:25:44 +0200

    add logging of failed logins

---
+++ app/failedLoginAudit/failed_login_audit.js
@@ -0,0 +1,17 @@
+import { callbacks } from '../callbacks';
+
+callbacks.add('beforeValidateLogin', (login) => {
+    if (login.allowed !== true) {
+        let user = 'unknown';
+        if (login.user !== undefined) {
+            user = login.user.username;
+        }
+        const { connection } = login;
+        let { clientAddress } = connection.clientAddress;
+        let forwardedFor = connection.httpHeaders['x-forwarded-for'];
+        let userAgent = connection.httpHeaders['user-agent'];
+        console.log('Failed login detected - Username[%s] ClientAddress[%s] ForwardedFor[%s] UserAgent[%s]', user, clientAddress, forwardedFor, userAgent);
+    }
+    return login;
+    }
+);
