8ba52e5a0592913ffc21b692f36e3c50e0184920
Martin Schröder <mschroeder@tyntec.com>
2019-07-10 10:20:08 +0200

    fix formatting problems eslint detected

--- app/failedLoginAudit/failed_login_audit.js
+++ app/failedLoginAudit/failed_login_audit.js
@@ -1,17 +1,19 @@
 import { callbacks } from '../callbacks';
 
 callbacks.add('beforeValidateLogin', (login) => {
-    if (login.allowed !== true) {
-        let user = 'unknown';
-        if (login.user !== undefined) {
-            user = login.user.username;
-        }
-        const { connection } = login;
-        let { clientAddress } = connection.clientAddress;
-        let forwardedFor = connection.httpHeaders['x-forwarded-for'];
-        let userAgent = connection.httpHeaders['user-agent'];
-        console.log('Failed login detected - Username[%s] ClientAddress[%s] ForwardedFor[%s] UserAgent[%s]', user, clientAddress, forwardedFor, userAgent);
-    }
-    return login;
-    }
+	if (login.allowed !== true) {
+		let user = 'unknown';
+		if (login.user !== undefined) {
+			user = login.user.username;
+		}
+		const { connection } = login;
+		const { clientAddress } = connection.clientAddress;
+		const forwardedFor = connection.httpHeaders['x-forwarded-for'];
+		const userAgent = connection.httpHeaders['user-agent'];
+		console.log(
+			'Failed login detected - Username[%s] ClientAddress[%s] ForwardedFor[%s] UserAgent[%s]',
+			user, clientAddress, forwardedFor, userAgent);
+	}
+	return login;
+}
 );
